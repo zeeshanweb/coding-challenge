f9741682d7804378e457136cf3d3fafcdd26ca3d
Zeeshan Khan <khanzeeshan872@gmail.com>
2021-10-16 17:05:09 +0530

    Some code refactor and text formatiing

--- php/Block.php
+++ php/Block.php
@@ -73,7 +73,7 @@ class Block {
 			<?php
 			foreach ( $post_types as $post_type_slug ) {
 				$post_type_object = get_post_type_object( $post_type_slug );
-				$post_count       = ( 'media' === $post_type_slug ) ? wp_count_posts( $post_type_slug )->inherit : wp_count_posts( $post_type_slug )->publish;
+				$post_count       = ( 'attachment' === $post_type_slug ) ? wp_count_posts( $post_type_slug )->inherit : wp_count_posts( $post_type_slug )->publish;
 				?>
 				<li>
 				<?php
@@ -87,48 +87,62 @@ class Block {
 			</ul>
 			<p>
 				<?php
-				$post_id = isset( $_GET['post_id'] ) ? wp_unslash( $_GET['post_id'] ) : ''; // phpcs:ignore
+				$post_id = ! empty( get_the_ID() ) ? get_the_ID() : 0;
 				/* translators: %d is post id */
 				echo sprintf( esc_html__( 'The current post ID is %d', 'site-counts' ), absint( $post_id ) );
 				?>
 			</p>
 			<?php
-				$query = new WP_Query(
+				$posts_per_page    = 5;
+				$meta_value_filter = 'Accepted';
+				$tag_filter        = 'foo';
+				$cat_filter        = 'baz';
+				$post_after_hour   = 9;
+				$post_before_hour  = 17;
+				$query             = new WP_Query(
 					array(
 						'post_type'      => array( 'post', 'page' ),
-						'posts_per_page' => 5,
+						'posts_per_page' => $posts_per_page,
 						'post_status'    => 'any',
 						'date_query'     => array(
 							array(
-								'hour'    => 9,
+								'hour'    => $post_after_hour,
 								'compare' => '>=',
 							),
 							array(
-								'hour'    => 17,
+								'hour'    => $post_before_hour,
 								'compare' => '<=',
 							),
 						),
-						'tag'            => 'foo',
-						'category_name'  => 'baz',
-						'post__not_in'   => array( get_the_ID() ),
-						'meta_value'     => 'Accepted',
+						'tag'            => $tag_filter,
+						'category_name'  => $cat_filter,
+						'post__not_in'   => array( $post_id ),
+						'meta_value'     => $meta_value_filter,
 					)
 				);
-			if ( $query->found_posts ) {
+			if ( $query->have_posts() ) :
 				?>
-				<h2><?php esc_html_e( 'Any 5 posts with the tag of foo and the category of baz where the custom field value is ‘Accepted’, regardless of the custom field key and posts between 9AM to 5PM', 'site-counts' ); ?></h2>
+				<h2>
+				<?php
+				/* translators: 1: total no of posts 2: tag name 3: category name 4: meta value 5: pots after hour 6: post before hour */
+				echo sprintf( esc_html__( 'Any %1$d posts with the tag of %2$s and the category of %3$s where the custom field value is %4$s, regardless of the custom field key and posts between %5$sAM to %6$sPM', 'site-counts' ), absint( $posts_per_page ), esc_html( $tag_filter ), esc_html( $cat_filter ), esc_html( $meta_value_filter ), esc_html( $post_after_hour ), esc_html( $post_before_hour ) );
+				?>
+				</h2>
 				<ul>
 				<?php
-				foreach ( $query->posts as $post ) {
+				while ( $query->have_posts() ) :
+					$query->the_post();
 					?>
-					<li><?php echo esc_html( $post->post_title ); ?></li>
+					<li><?php the_title(); ?></li>
 					<?php
-				}
-				?>
+				endwhile; else :
+					?>
+				<p><?php esc_html_e( 'Sorry, no posts matched your criteria.', 'site-counts' ); ?></p>
 				</ul>
-				<?php
-			}
-			?>
+					<?php
+			endif;
+				wp_reset_postdata();
+				?>
 		</div>
 		<?php
 		return ob_get_clean();
