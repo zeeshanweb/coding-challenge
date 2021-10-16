c569246d17c7b84dfe6621e7e326606e34d00005
Zeeshan Khan <khanzeeshan872@gmail.com>
2021-10-16 15:34:42 +0530

    Added translation support for teh text and some code refractor and also added the PHPCS coding standard to the code

--- php/Block.php
+++ php/Block.php
@@ -63,67 +63,74 @@ class Block {
 	 * @return string The markup of the block.
 	 */
 	public function render_callback( $attributes, $content, $block ) {
-		$post_types = get_post_types(  [ 'public' => true ] );
-		$class_name = $attributes['className'];
+		$post_types = get_post_types( array( 'public' => true ) );
+		$class_name = isset( $attributes['className'] ) ? $attributes['className'] : '';
 		ob_start();
-
 		?>
-        <div class="<?php echo $class_name; ?>">
-			<h2>Post Counts</h2>
+		<div class="<?php echo esc_attr( $class_name ); ?>">
+			<h2><?php esc_html_e( 'Post Counts:', 'site-counts' ); ?></h2>
 			<ul>
 			<?php
-			foreach ( $post_types as $post_type_slug ) :
-                $post_type_object = get_post_type_object( $post_type_slug  );
-                $post_count = count(
-                    get_posts(
-						[
-							'post_type' => $post_type_slug,
-							'posts_per_page' => -1,
-						]
-					)
-                );
-
+			foreach ( $post_types as $post_type_slug ) {
+				$post_type_object = get_post_type_object( $post_type_slug );
+				$post_count       = ( 'media' === $post_type_slug ) ? wp_count_posts( $post_type_slug )->inherit : wp_count_posts( $post_type_slug )->publish;
 				?>
-				<li><?php echo 'There are ' . $post_count . ' ' .
-					  $post_type_object->labels->name . '.'; ?></li>
-			<?php endforeach;	?>
-			</ul><p><?php echo 'The current post ID is ' . $_GET['post_id'] . '.'; ?></p>
-
-			<?php
-			$query = new WP_Query(  array(
-				'post_type' => ['post', 'page'],
-				'post_status' => 'any',
-				'date_query' => array(
-					array(
-						'hour'      => 9,
-						'compare'   => '>=',
-					),
-					array(
-						'hour' => 17,
-						'compare'=> '<=',
-					),
-				),
-                'tag'  => 'foo',
-                'category_name'  => 'baz',
-				  'post__not_in' => [ get_the_ID() ],
-				  'meta_value' => 'Accepted',
-			));
-
-			if ( $query->found_posts ) :
+				<li>
+				<?php
+				/* translators: 1: post count 2: post type label name */
+				echo sprintf( esc_html__( 'There are %1$d %2$s', 'site-counts' ), absint( $post_count ), esc_html( $post_type_object->labels->name ) );
 				?>
-				 <h2>Any 5 posts with the tag of foo and the category of baz</h2>
-                <ul>
-                <?php
-
-                 foreach ( array_slice( $query->posts, 0, 5 ) as $post ) :
-                    ?><li><?php echo $post->post_title ?></li><?php
-				endforeach;
-			endif;
-		 	?>
+				</li>
+				<?php
+			}
+			?>
 			</ul>
+			<p>
+				<?php
+				$post_id = isset( $_GET['post_id'] ) ? wp_unslash( $_GET['post_id'] ) : ''; // phpcs:ignore
+				/* translators: %d is post id */
+				echo sprintf( esc_html__( 'The current post ID is %d', 'site-counts' ), absint( $post_id ) );
+				?>
+			</p>
+			<?php
+				$query = new WP_Query(
+					array(
+						'post_type'      => array( 'post', 'page' ),
+						'posts_per_page' => 5,
+						'post_status'    => 'any',
+						'date_query'     => array(
+							array(
+								'hour'    => 9,
+								'compare' => '>=',
+							),
+							array(
+								'hour'    => 17,
+								'compare' => '<=',
+							),
+						),
+						'tag'            => 'foo',
+						'category_name'  => 'baz',
+						'post__not_in'   => array( get_the_ID() ),
+						'meta_value'     => 'Accepted',
+					)
+				);
+			if ( $query->found_posts ) {
+				?>
+				<h2><?php esc_html_e( 'Any 5 posts with the tag of foo and the category of baz where the custom field value is ‘Accepted’, regardless of the custom field key and posts between 9AM to 5PM', 'site-counts' ); ?></h2>
+				<ul>
+				<?php
+				foreach ( $query->posts as $post ) {
+					?>
+					<li><?php echo esc_html( $post->post_title ); ?></li>
+					<?php
+				}
+				?>
+				</ul>
+				<?php
+			}
+			?>
 		</div>
 		<?php
-
 		return ob_get_clean();
 	}
 }
